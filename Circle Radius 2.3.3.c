32a94baed92c2da06cbcb9d0fc85e463844323ac
Chris Riccardi <cricc33690@gmail.com>
2014-03-16 20:41:06 -0400

    Program in C
    Programs from the Text

---
+++ Circle Radius 2.3.3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define PI 3.14159
+
+int
+main (void)
+{
+	double Radius, Area
+	printf("Please enter the radius of the circle > \n");
+	scanf("%lf", Radius);
+	Area = PI * Radius * Radius
+	printf("The circle's area is %f", Area);
+
+	return (0);
+
+}
---
+++ Three Integers 2.3.1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int first, second, third;
+
+	printf("Please type in your three integers seperated by spaces.\n");
+	scanf("%d%d%d",first, second, third);
+
+return (0);
+}
